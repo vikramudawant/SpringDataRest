851c5b27b280fbbf9043b15be28078be98f186fe
vikram <vikram@LAPTOP-IC4HBLJQ>
2020-05-12 11:54:00 +0530

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'navin', 'java');
+insert into alien values(102,'pankaj', '.net');
+insert into alien values(103,'sushant', 'Android');
+insert into alien values(104,'priti', 'PHP');
+insert into alien values(105,'Suhas', 'C');
